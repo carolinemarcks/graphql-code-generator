606eab33e65a823d8e33761c04e51829bb4d681e
Caroline Marcks <caroline.marcks@gmail.com>
2018-10-17 02:46:50 +0000

    add flow template

---
+++ packages/templates/flow/src/config.ts
@@ -0,0 +1,26 @@
+import * as index from './template.handlebars';
+import * as type from './type.handlebars';
+import * as schema from './schema.handlebars';
+import * as documents from './documents.handlebars';
+import * as selectionSet from './selection-set.handlebars';
+import { EInputType, GeneratorConfig } from 'graphql-codegen-core';
+
+export const config: GeneratorConfig = {
+  inputType: EInputType.SINGLE_FILE,
+  templates: {
+    index,
+    type,
+    schema,
+    documents,
+    selectionSet
+  },
+  flattenTypes: true,
+  primitives: {
+    String: 'string',
+    Int: 'number',
+    Float: 'number',
+    Boolean: 'boolean',
+    ID: 'string'
+  },
+  outFile: 'schemaTypes.js'
+};
